8717db097dbba0f1a22909e8adaae9a3c76efe29
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-04 16:52:11 +0000

    Exported the modular function and created http-test.js to test it

---
+++ http-functions.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getHTML(request, callback) {
+
+  https.get(requestOptions, function(response) {
+    console.log("Started request");
+
+    var chunkData = '';
+
+    response.on('data', function(data){
+      chunkData += data;
+    });
+
+    response.on('end', function() {
+      console.log('Finished receiving data')
+      callback(chunkData);
+    })
+  });
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+function printHTML(html) {
+  console.log(html);
+}
+
+module.exports = { getHTML };
